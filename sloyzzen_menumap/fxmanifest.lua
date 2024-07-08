de782b22cc51784df19aef8627d59ca10b4117ef
SloyzzenDev <sloyzzendev@hotmail.com>
2024-07-07 22:25:32 -0400

    Add files via upload

---
+++ sloyzzen_menumap/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Sloyzzen Dev'
+description 'this script is designed so that when you open your map you have the map in your hands'
+version '1.0.0'
+
+client_scripts {
+    'map.lua' 
+}
---
+++ sloyzzen_menumap/map.lua
@@ -0,0 +1,45 @@
+local isPauseMenuActive = false
+local mapProp = "prop_tourist_map_01"
+local mapPropObject = nil 
+function startMapAnimation()
+    local playerPed = PlayerPedId()
+
+    RequestModel(mapProp)
+    while not HasModelLoaded(mapProp) do
+        Wait(1)
+    end
+
+    RequestAnimDict("amb@world_human_tourist_map@male@base")
+    while not HasAnimDictLoaded("amb@world_human_tourist_map@male@base") do
+        Wait(1)
+    end
+
+    mapPropObject = CreateObject(GetHashKey(mapProp), 0, 0, 0, true, true, true) -- Store the prop object globally
+    local rightHandIndex = GetPedBoneIndex(playerPed, 57005)
+    AttachEntityToEntity(mapPropObject, playerPed, rightHandIndex, 0.09, 0.02, -0.08, -80.0, 0.0, 0.0, true, true, false, true, 1, true)
+    
+    TaskPlayAnim(playerPed, "amb@world_human_tourist_map@male@base", "base", 8.0, -8.0, -1, 49, 0, false, false, false)
+end
+
+function stopMapAnimation()
+    ClearPedTasks(PlayerPedId())
+    RemoveAnimDict("amb@world_human_tourist_map@male@base")
+    if mapPropObject and DoesEntityExist(mapPropObject) then
+        DeleteObject(mapPropObject)
+        mapPropObject = nil
+    end
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(300)
+
+        if IsPauseMenuActive() and not isPauseMenuActive then
+            isPauseMenuActive = true
+            startMapAnimation()
+        elseif not IsPauseMenuActive() and isPauseMenuActive then
+            isPauseMenuActive = false
+            stopMapAnimation()
+        end
+    end
+end)
